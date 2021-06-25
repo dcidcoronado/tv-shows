8d9c75e8033a6400a7157e9ce9fae2353db7b080
dcidcoronado <daniel.cid.coronado@gmail.com>
2021-06-25 13:07:15 -0400

    validatios+bonus

--- tv_shows_app/models.py
+++ tv_shows_app/models.py
@@ -1,18 +1,44 @@
 from django.db import models
+import re
+from datetime import datetime
 
 # Create your models here.
 class ShowManager(models.Manager):
     def basic_validator(self, postData):
         errors = {}
-        if len(postData['title']) < 2:
-            errors['title'] = "The name of the show must be at least 2 characters long";
-        
-        if len(postData['network']) < 3:
-            errors['network'] = "The network of the show must be at least 3 characters long";
+
+        TITLE_REGEX = re.compile(r'^[A-Z][a-zA-Z0-9\s]+$')
+        if postData['title']:
+            if not TITLE_REGEX.match(postData['title']):    
+                errors['title'] = "Title must start with a capital letter!"
+            elif len(postData['title']) < 2:
+                errors['title'] = "The name of the show must be at least 2 characters long"
+            elif Show.objects.filter(title=postData['title']) :
+                errors['title'] = "Must be a new show"
+        if not postData['title']:
+            errors['title'] = "Must enter a Title"        
+                
+        NETWORK_REGEX = re.compile(r'^[A-Z][a-zA-Z0-9\s]+$')
+        if postData['title']:
+            if not NETWORK_REGEX.match(postData['network']):    
+                errors['network'] = "Network must start with a capital letter!"        
+            elif len(postData['network']) < 3:
+                errors['network'] = "The network of the show must be at least 3 characters long"
+        if not postData['network']:
+            errors['network'] = "Must enter a Network"
+
+        if postData['date']:
+            if datetime.strptime(postData['date'],'%Y-%m-%d') > datetime.now():
+                errors['date'] = "Release date must be in the past"
+        if not postData['date']:
+            errors['date'] = "Must enter a Release date"
+
+        DESC_REGEX = re.compile(r'^[A-Z][a-zA-Z0-9\s]+$')
         if len(postData['description']) != 0: 
             if len(postData['description']) < 10:
-                errors['description'] = "The description of the show must be at least 10 characters long";
-        
+                errors['description'] = "The description of the show must be at least 10 characters long"
+            if not DESC_REGEX.match(postData['description']):    
+                errors['description'] = "Description must start with a capital letter!"
         return errors
 
 
@@ -23,6 +49,7 @@ class Show(models.Model):
     description = models.TextField(null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
     objects = ShowManager()
 
     def __repr__(self): 
